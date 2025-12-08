824cea94e0ca3484c1fe170095ea70001d44cff4
Yehuda Hason <yehudaxt@gmail.com>
2025-12-08 03:53:10 +0200

    first commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -64,7 +64,6 @@ export default function About() {
           <input
             type="image"
             src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-            border="0"
             name="submit"
             title="PayPal - The safer, easier way to pay online!"
             alt="Donate with PayPal button"
